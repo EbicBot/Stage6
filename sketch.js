3e4a883206dd4db9794da63909fa8a64ab86cf06
Varun <vjayaram2006@gmail.com>
2020-07-27 21:13:20 -0400

    commit on master

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-
 var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
